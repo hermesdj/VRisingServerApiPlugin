2c63475e0b3ab4018777de4520445baa8eff7ef7
Jay's <dallardj@gmail.com>
2023-06-14 10:50:41 +0200

    Cleared github files from library and created singleton for Http security handling

---
+++ http/security/AuthorizedUser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace VRisingServerApiPlugin.http.security;
+
+public class AuthorizedUser
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    private AuthorizedUser(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+
+    public static IEnumerable<AuthorizedUser> ParseConfig(string authorizedUsers)
+    {
+        return (from user in authorizedUsers.Split(",")
+            select user.Split(':')
+            into parts
+            where parts.Length == 2
+            select new AuthorizedUser(parts[0].Trim(), parts[1].Trim())).ToList();
+    }
+}
---
+++ http/security/HttpSecuritySingleton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BepInEx.Configuration;
+
+namespace VRisingServerApiPlugin.http.security;
+
+public class HttpSecuritySingleton
+{
+    private static HttpSecuritySingleton _instance;
+
+    private IEnumerable<AuthorizedUser> _authorizedUsers;
+
+    public static HttpSecuritySingleton GetInstance()
+    {
+        return _instance ??= new HttpSecuritySingleton();
+    }
+
+    public void Initialize(ConfigEntry<string> authorizedUsers)
+    {
+        authorizedUsers.SettingChanged += (_, args) =>
+        {
+            var changedArgs = (SettingChangedEventArgs)args;
+            var changedSettings = (ConfigEntry<string>)changedArgs.ChangedSetting;
+
+            _authorizedUsers = GetAuthorizedUserList(changedSettings.Value);
+        };
+        _authorizedUsers = GetAuthorizedUserList(authorizedUsers.Value);
+    }
+
+    private static IEnumerable<AuthorizedUser> GetAuthorizedUserList(string authorizedUsers)
+    {
+        return AuthorizedUser.ParseConfig(authorizedUsers);
+    }
+
+    public bool CheckAuthenticationOfUser(string username, string password)
+    {
+        return _authorizedUsers
+            .Count(user => user.Username.Equals(username) && user.Password.Equals(password)) == 1;
+    }
+}
